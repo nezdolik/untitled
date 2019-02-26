4dc6eb3b24fe505b3318f2c64d2a94e253defc0b
Kateryna Nezdolii <nezdolik@spotify.com>
2019-02-26 15:58:57 +0100

    add fallback

--- subset_lb.cpp
+++ subset_lb.cpp
@@ -4,9 +4,14 @@
 
 SubsetLoadBalancer::SubsetLoadBalancer() :
         subset_keys_top_({
-                             {"version", "env"},
-                             {"app",     "version"},
-                             {"x"}
+                                 std::pair<std::set<std::string>, std::optional<Fallback>>({"version", "env"},
+                                                                                           std::optional<Fallback>(
+                                                                                                   Fallback::Default)),
+                                 std::pair<std::set<std::string>, std::optional<Fallback>>({"app", "version"},
+                                                                                           std::optional<Fallback>()),
+                                 std::pair<std::set<std::string>, std::optional<Fallback>>({"x"},
+                                                                                           std::optional<Fallback>(
+                                                                                                   Fallback::None))
                          }) {
     initSubsetSelectorMap();
 };
@@ -16,12 +21,19 @@ SubsetLoadBalancer::~SubsetLoadBalancer() {};
 
 void SubsetLoadBalancer::initSubsetSelectorMap() {
     SubsetSelectorMap *selectors = &selectors_;
-    for (const auto &selector_keys : subset_keys_top_) {
+    for (const auto &selector_with_fallback : subset_keys_top_) {
+        const auto &selector_keys = selector_with_fallback.first;
+        int pos = 0;
         for (const auto &key : selector_keys) {
             const auto &selector_it = selectors->subset_keys.find(key);
             if (selector_it == selectors->subset_keys.end()) {
-                selectors->subset_keys.emplace(std::pair<std::string, SubsetSelectorMap>(key, SubsetSelectorMap()));
+                //if this is last key for given selector, check if it has fallback specified
+                if (++pos == selector_keys.size() && selector_with_fallback.second) {
+                    selectors->fallback = selector_with_fallback.second;
+                }
+                selectors->subset_keys.emplace(std::make_pair(key, SubsetSelectorMap()));
                 selectors = &selectors->subset_keys.find(key)->second;
+
             } else {
                 selectors = &selector_it->second;
             };
@@ -31,21 +43,29 @@ void SubsetLoadBalancer::initSubsetSelectorMap() {
 }
 
 void SubsetLoadBalancer::printSubsetKeys() {
-    for (auto i = subset_keys_top_.begin(); i != subset_keys_top_.end(); ++i) {
-        for (const auto key : *i) {
-            std::cout << key << ' ';
-        }
-        std::cout << std::endl;
-    }
+//    for (auto i = subset_keys_top_.begin(); i != subset_keys_top_.end(); ++i) {
+//        for (const auto key : *i) {
+//            std::cout << key << ' ';
+//        }
+//        std::cout << std::endl;
+//    }
 }
 
 void SubsetLoadBalancer::printSelectors() {
 
     SubsetSelectorMap *selectors = &selectors_;
-    for (const auto &selector_keys : subset_keys_top_) {
+    for (const auto &selector_with_fallback : subset_keys_top_) {
+        const auto &selector_keys = selector_with_fallback.first;
+        int pos = 0;
         for (const auto &key : selector_keys) {
             const auto &selector_it = selectors->subset_keys.find(key);
             std::cout << key << ' ';
+            //if this is last key for given selector, check if it has fallback specified
+            if (++pos == selector_keys.size() && selector_with_fallback.second) {
+                std::cout << "<Fallback: " << (selector_with_fallback.second.value() == Fallback::Any ? "Any>" :
+                                               selector_with_fallback.second.value() == Fallback::Default ? "Default>"
+                                                                                                          : "None>");
+            }
             selectors = &selector_it->second;
         };
         std::cout << std::endl;
--- subset_lb.h
+++ subset_lb.h
@@ -1,24 +1,41 @@
 
 #pragma once
+
 #include <unordered_map>
 #include <set>
 #include <vector>
 #include <string>
+#include  <optional>
+
+
+
+enum class Fallback {
+    None,
+    Any,
+    Default
+};
 
 struct SubsetSelectorMap {
     std::unordered_map<std::string, SubsetSelectorMap> subset_keys;
+    std::optional<Fallback> fallback;
 };
 
 class SubsetLoadBalancer {
 private:
     SubsetSelectorMap selectors_;
-    const std::vector<std::set<std::string> > subset_keys_top_;
-    void doPrint(const SubsetSelectorMap map,const std::string* key);
+    const std::vector<std::pair<std::set<std::string>, std::optional<Fallback>>> subset_keys_top_;
+
+    void doPrint(const SubsetSelectorMap map, const std::string *key);
+
 public:
     SubsetLoadBalancer();
+
     ~SubsetLoadBalancer();
+
     void initSubsetSelectorMap();
+
     void printSubsetKeys();
+
     void printSelectors();
 };
 
